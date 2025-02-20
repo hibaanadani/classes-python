3ce2ee88584ef9eaf42c14d27fc703cefe1c3029
hibaanadani <hibaanadani1@gmail.com>
2025-02-20 22:55:23 +0200

    testing the dictionaries

--- Nabihas salary.py
+++ Nabihas salary.py
@@ -1,6 +1,5 @@
 income=[]
-salary=input("Enter this months salary: ")
-sum=0
+salary=int(input("Enter this months salary: "))
 while salary != "stop":
     month=input("Enter the month: ")
     salaries={
@@ -8,13 +7,17 @@ while salary != "stop":
         "month":month,
     }
     income.append(salaries)
-    savings=input("Enter savings percentage: ")
-    rent= input("Enter how much is paid for rent: ")
-    electricity= input("Enter electricity bill: ")
+    sum=0
+    expanses=[]
+    savings=int(input("Enter savings percentage: "))
+    rent= int(input("Enter how much is paid for rent: "))
+    electricity= int(input("Enter electricity bill: "))
     account ={
         "savings":salary*(savings/100),
         "rent":salary*(rent/100),
         "electricity":salary*(electricity/100),
     }
+    expanses.append(account)
+    print(f"this month expanses are {account['electricity']} for electricity, {account["rent"]} for rent, and {account["savings"]} towards savings")
     sum+=account["savings"]+account["rent"]+account["electricity"]
     salary=input("Enter this months salary: ")
