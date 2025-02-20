dde4196746c994f0730740732c27459f35c9e1cd
hibaanadani <hibaanadani1@gmail.com>
2025-02-20 22:34:52 +0200

    changing structure

--- Nabihas salary.py
+++ Nabihas salary.py
@@ -1,17 +1,20 @@
-salaries=[]
+income=[]
 salary=input("Enter this months salary: ")
-month=input("Enter the month: ")
-savings=input("Enter savings percentage: ")
-rent= input("Enter how much is paid for rent:")
-electricity= input("Enter electricity bill: ")
-
+sum=0
 while salary != "stop":
+    month=input("Enter the month: ")
     salaries={
         "salary":salary,
         "month":month,
     }
+    income.append(salaries)
+    savings=input("Enter savings percentage: ")
+    rent= input("Enter how much is paid for rent: ")
+    electricity= input("Enter electricity bill: ")
     account ={
         "savings":salary*(savings/100),
         "rent":salary*(rent/100),
         "electricity":salary*(electricity/100),
     }
+    sum+=account["savings"]+account["rent"]+account["electricity"]
+    salary=input("Enter this months salary: ")
