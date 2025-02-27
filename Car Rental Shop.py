786218af4c6ec171d9ba3fa9ac2498eac8330307
hibaanadani <hibaanadani1@gmail.com>
2025-02-27 20:09:26 +0200

    Bobs Rental car init class

---
+++ Car Rental Shop.py
@@ -0,0 +1,22 @@
+class carRental:
+    def __init__(self,brand,model,year,price,seating):
+        self.brand=''
+        self.model=''
+        self.year=0
+        self.price=0
+        self.seating=0
+
+def prompt():
+    print("Select from the options below: ")
+    print("1. Display vehicle details")
+    print("2. Display available motorcycles")
+    print("1. Display vehicle details")
+    def calculate_rental_cost(self,days):
+        if days>=1:
+            rental_cost=days*self.price
+        return rental_cost
+    
+    def additional_attribute(self):
+        print(f"The car {self.model} has {self.seating}")
+
+    
