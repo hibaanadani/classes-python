a6ddd747ae3d333cb64e3bebfe305a96c05644fe
hibaanadani <hibaanadani1@gmail.com>
2025-02-27 22:13:54 +0200

    removed the prompt and fixed the attributes

--- Car Rental Shop.py
+++ Car Rental Shop.py
@@ -1,17 +1,15 @@
 class Vehical:
     def __init__(self,brand,model,year,price):
-        self.brand=''
-        self.model=''
-        self.year=0
-        self.price=0
+        self.brand=brand
+        self.model=model
+        self.year=year
+        self.price=price
 
     def display_info(self):
         print(f"The car is {self.brand} {self.model}, Year: {self.year},Rental Price: {self.price}")  
     
     def calculate_rental_cost(self,days):
-        if days>=1:
-            rental_cost=days*self.price
-        return rental_cost
+        return days*self.price
     
     def set_rental_price_per_day(self,price):
         self.price=price
@@ -19,15 +17,8 @@ class Vehical:
     def get_rental_price(self):
         return self.price
     
-    def additional_attribute(self):
-        print(f"The car {self.model} has {self.seating}")
 
 
-def prompt():
-    print("Select from the options below: ")
-    print("1. Display vehicle details")
-    print("2. Display Rental costs")
-    print("1. Display vehicle details")
 
     
     
