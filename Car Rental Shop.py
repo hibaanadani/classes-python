7f44f65ac37b8199415e3ef5c2e3ab8e52334627
hibaanadani <hibaanadani1@gmail.com>
2025-02-27 22:20:24 +0200

    added class car and method override display info

--- Car Rental Shop.py
+++ Car Rental Shop.py
@@ -6,7 +6,7 @@ class Vehical:
         self.price=price
 
     def display_info(self):
-        print(f"The car is {self.brand} {self.model}, Year: {self.year},Rental Price: {self.price}")  
+        print(f"{self.brand} {self.model}, Year: {self.year},Rental Price: {self.price}")  
     
     def calculate_rental_cost(self,days):
         return days*self.price
@@ -17,8 +17,13 @@ class Vehical:
     def get_rental_price(self):
         return self.price
     
+class Car(Vehical):
+    def __init__(self, brand, model, year, price, seating_capacity):
+        super().__init__(brand, model, year, price)
+        self.seating_capacity=seating_capacity
 
-
+    def override_display_info(self):
+        print(f"{super().display_info}, seats: {self.seating_capacity}")
 
     
     
