e40c58aaa3ca6c8c0ae13aadcdfd842f68eb2388
hibaanadani <hibaanadani1@gmail.com>
2025-02-28 15:48:23 +0200

    setting rental price as private

--- Car Rental Shop.py
+++ Car Rental Shop.py
@@ -3,10 +3,10 @@ class Vehical:
         self.brand=brand
         self.model=model
         self.year=year
-        self.price=price
+        self.__price=price
 
     def display_info(self):
-        print(f"{self.brand} {self.model}, Year: {self.year},Rental Price: {self.price}")  
+        print(f"{self.brand} {self.model}, Year: {self.year},Rental Price: {self.__price}")  
     
     def calculate_rental_cost(self,days):
         return days*self.price
@@ -14,26 +14,26 @@ class Vehical:
     def set_rental_price_per_day(self,price):
         self.price=price
 
-    def get_rental_price(self):
+    def get_rental_price_per_day(self):
         return self.price
     
 class Car(Vehical):
-    def __init__(self, brand, model, year, price, seating_capacity,engine_capacity):
-        super().__init__(brand, model, year, price)
+    def __init__(self, brand, model, year, __price, seating_capacity,engine_capacity):
+        super().__init__(brand, model, year, __price)
         self.seating_capacity=seating_capacity
         self.engine_capacity=engine_capacity
 
     def display_info(self):
-        print(f"{super().display_info}, seats: {self.seating_capacity}, engine:{self.engine_capacity}")
+        print(f"Car: {super().display_info}, seats: {self.seating_capacity}, engine:{self.engine_capacity}")
 
 class Motorcycle(Vehical):
-    def __init__(self, brand, model, year, price, seating_capacity,engine_capacity):
-        super().__init__(brand, model, year, price)
+    def __init__(self, brand, model, year, __price, seating_capacity,engine_capacity):
+        super().__init__(brand, model, year, __price)
         self.seating_capacity=seating_capacity
         self.engine_capacity=engine_capacity
 
     def display_info(self):
-        print(f"{super().display_info}, seats: {self.seating_capacity}, engine:{self.engine_capacity}")
+        print(f"Motorcycle: {super().display_info}, seats: {self.seating_capacity}, engine:{self.engine_capacity}")
      
 def show_vehicle_info(vehical):
-    return vehical.display_info
+    return vehical.display_info()
