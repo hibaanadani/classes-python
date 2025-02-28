001ed0a47f9421f8a37c18c431fe6f4ce73e5a59
hibaanadani <hibaanadani1@gmail.com>
2025-02-28 16:12:50 +0200

    fixed naming and return instead of print in display

--- Car Rental Shop.py
+++ Car Rental Shop.py
@@ -1,4 +1,4 @@
-class Vehical:
+class Vehicle:
     def __init__(self,brand,model,year,price):
         self.brand=brand
         self.model=model
@@ -6,7 +6,7 @@ class Vehical:
         self.__price=price
 
     def display_info(self):
-        print(f"{self.brand} {self.model}, Year: {self.year},Rental Price: {self.__price}")  
+        return f"{self.brand} {self.model}, Year: {self.year},Rental Price: {self.__price}"
     
     def calculate_rental_cost(self,days):
         return days*self.price
@@ -17,23 +17,24 @@ class Vehical:
     def get_rental_price_per_day(self):
         return self.__price
     
-class Car(Vehical):
-    def __init__(self, brand, model, year, __price, seating_capacity,engine_capacity):
+class Car(Vehicle):
+    def __init__(self, brand, model, year, __price, seating_capacity):
         super().__init__(brand, model, year, __price)
         self.seating_capacity=seating_capacity
+
+    def display_info(self):
+        return f"Car: {super().display_info}, seats: {self.seating_capacity}"
+
+class Motorcycle(Vehicle):
+    def __init__(self, brand, model, year, __price,engine_capacity):
+        super().__init__(brand, model, year, __price)
         self.engine_capacity=engine_capacity
 
     def display_info(self):
-        print(f"Car: {super().display_info}, seats: {self.seating_capacity}, engine:{self.engine_capacity}")
-
-class Motorcycle(Vehical):
-    def __init__(self, brand, model, year, __price, seating_capacity,engine_capacity):
-        super().__init__(brand, model, year, __price)
-        self.seating_capacity=seating_capacity
-        self.engine_capacity=engine_capacity
-
-    def display_info(self):
-        print(f"Motorcycle: {super().display_info}, seats: {self.seating_capacity}, engine:{self.engine_capacity}")
+        return f"Motorcycle: {super().display_info},engine:{self.engine_capacity}"
      
-def show_vehicle_info(vehical):
-    return vehical.display_info()
+def show_vehicle_info(vehicle):
+    return vehicle.display_info()
+
+toyota = Car("Toyota","Corolla",2020,50,5)
+toyota.display_info()
