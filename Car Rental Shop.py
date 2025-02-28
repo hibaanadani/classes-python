ba2892cb446367967919426f9c272190f55f0268
hibaanadani <hibaanadani1@gmail.com>
2025-02-28 16:18:43 +0200

    fixed the output

--- Car Rental Shop.py
+++ Car Rental Shop.py
@@ -9,7 +9,7 @@ class Vehicle:
         return f"{self.brand} {self.model}, Year: {self.year},Rental Price: {self.__price}"
     
     def calculate_rental_cost(self,days):
-        return days*self.price
+        return days*self.__price
     
     def set_rental_price_per_day(self,price):
         self.__price=price
@@ -18,23 +18,23 @@ class Vehicle:
         return self.__price
     
 class Car(Vehicle):
-    def __init__(self, brand, model, year, __price, seating_capacity):
-        super().__init__(brand, model, year, __price)
+    def __init__(self, brand, model, year, price, seating_capacity):
+        super().__init__(brand, model, year, price)
         self.seating_capacity=seating_capacity
 
     def display_info(self):
-        return f"Car: {super().display_info}, seats: {self.seating_capacity}"
+        return f"Car: {super().display_info()}, seats: {self.seating_capacity}"
 
 class Motorcycle(Vehicle):
-    def __init__(self, brand, model, year, __price,engine_capacity):
-        super().__init__(brand, model, year, __price)
+    def __init__(self, brand, model, year, price,engine_capacity):
+        super().__init__(brand, model, year, price)
         self.engine_capacity=engine_capacity
 
     def display_info(self):
-        return f"Motorcycle: {super().display_info},engine:{self.engine_capacity}"
+        return f"Motorcycle: {super().display_info()},engine:{self.engine_capacity}"
      
 def show_vehicle_info(vehicle):
-    return vehicle.display_info()
+    print (vehicle.display_info())
 
 toyota = Car("Toyota","Corolla",2020,50,5)
-toyota.display_info()
+show_vehicle_info(toyota)
